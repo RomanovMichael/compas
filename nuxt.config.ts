1c9d44d90de5f72c295cc875c9a48e7a7234c439
Romanov Michael <romanov.players@gmail.com>
2024-06-13 13:39:39 +0300

    feat(ui-kit): add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: [
+    '~/styles/main.scss',
+    '~/styles/variables.scss',
+    '~/styles/fonts.scss',
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/mixins.scss" as *;',
+        },
+      },
+    },
+    plugins: [
+      // svgLoader(),
+    ],
+  },
+  modules: [
+    '@pinia/nuxt',
+  ],
 })
