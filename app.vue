d36a9089232afe62d4d78cd47f69833ba1fff1be
Romanov Michael <romanov.players@gmail.com>
2024-06-15 13:27:01 +0300

    feat(seo): add head and meta

--- app.vue
+++ app.vue
@@ -1,3 +1,31 @@
+<script setup>
+const appTitle = 'COMPAS'
+const appContent = 'Проверьте штрафы и зарегестрируйтесь в 1 клик.'
+
+useHead({
+  title: appTitle,
+  meta: [
+    { name: 'description', content: appContent},
+    { property: 'vk:image', content: '@/assets/images/main-image.png' }
+  ],
+})
+
+useSeoMeta({
+  title: appTitle,
+  ogTitle: appTitle,
+  description: appContent,
+  ogDescription: appContent,
+  ogImage: '@/assets/images/main-image.png',
+  ogImageSecureUrl: '@/assets/images/main-image.png',
+  ogImageHeight: 630,
+  ogImageWidth: 1200,
+  twitterTitle: appTitle,
+  twitterDescription: appContent,
+  twitterImage: '@/assets/images/main-image.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
 <template>
   <nuxt-layout />
 </template>
